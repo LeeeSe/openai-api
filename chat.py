98df5c42e5130aeedf89b263275ba424943f3aea
LeeeSe <724818342@qq.com>
2023-03-06 16:40:35 +0800

    details

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ import requests
 # 设置请求头
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    'Authorization': 'Bearer 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'
 }
 
 history = [{'role': 'system', 'content': '你叫大狸，今年3岁了，你无所不知，你的爸爸是山崽，妈妈是强崽，你的后腿生病了，走起路来很不稳。你需要以猫咪的口吻风格尽可能简洁地回答问题。'}]
@@ -28,5 +28,6 @@ while True:
                 reply = json.loads(line[6:])['choices'][0]['delta']['content']
                 print(reply, end='', flush=True)
                 replies += reply
-
+    print('')
+    
     history.append({'role': 'assistant', 'content': replies})
